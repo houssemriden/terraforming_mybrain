dafd5e281654756e245d06eac396ddcd2ce7e622
houssemriden <houssrid17@gmail.com>
2022-10-16 16:43:02 +0000

    ajout du user_data

--- main.tf
+++ main.tf
@@ -9,5 +9,16 @@ resource "aws_instance" "example" {
 
 	ami = " ami-xxx"
 	instance_type = "t1.micro"
+	tags { 
+		name="givename"
 
 }
+	user_data = << EOF 
+	#! /bin/bash
+	echo "Hello , world " > index.html
+	nohup busybox httpd -f -p 8080 &
+	EOF
+
+}
+
+	

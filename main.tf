17b9b6679dc3601e65bd9658eaf4d6c6f18d8b63
houssemriden <houssrid17@gmail.com>
2022-10-23 12:32:33 +0000

    ajout de gitignore

--- main.tf
+++ main.tf
@@ -1,35 +1,40 @@
 provider "aws" {
-	region = "us-east-1"
+	shared_credentials_files=["~/.aws/credentials"]
 }
 
-
-resource "aws_security_group" "instance" {
-	name="terraform-example-instance"
-	
-	ingress {
-		from_port = 8080	 
-		to_port = 8080
-		protocol = "tcp"
-		cidr_block = ["0.0.0.0/0"]
-	
+variable "port" {
+	description = "the port the server will use for http" 
+	default=8080
 }
+resource "aws_instance" "test" {
+	ami = "ami-0ddab716196087271"
+	instance_type="t2.micro"
+	vpc_security_group_ids =["${aws_security_group.sg_terraform.id}"]
 
-}
 
-resource "aws_instance" "example" {
-	vpc_security_groups_ids = ["${aws_security_group.instance.id}"]
-	ami = " ami-xxx"
-	instance_type = "t1.micro"
-	tags { 
-		name="givename"
-
-}
-	user_data = << EOF 
-	#! /bin/bash
-	echo "Hello , world " > index.html
-	nohup busybox httpd -f -p 8080 &
-	EOF
+	user_data = <<EOF
+	 	#! /bin/bash	
+		echo " hello,world" > index.html
+		nohup busybox httpd -f -p "${var.port}" &
+		EOF
+		
+	tags = {
+	name="terraform_configuration"
+	}
 
 }
+resource "aws_security_group" "sg_terraform" {
+	name="sg_terraform"
 
-	
+	ingress  { 
+		from_port ="${var.port}"
+		to_port="${var.port}"
+		protocol="tcp"
+		cidr_blocks=["0.0.0.0/0"]
+}
+}
+
+output "publicip"{
+	value="$test.public_ip"
+
+}

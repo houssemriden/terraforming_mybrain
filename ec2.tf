37e8c4725bc43f44bdbda094018bcab25effe337
houssemriden <houssrid17@gmail.com>
2023-04-26 11:30:18 +0200

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,4 +1,18 @@
 resource "aws_instance" "test" {
 	ami = "ami-06e46074ae430fba6"
 	instance_type="t2.micro"
+	count=2
 	vpc_security_group_ids =["${aws_security_group.sg_terraform.id}"]
+
+	
+	
+	resource "aws_security_group" "sg_terraform" {
+	name="sg_terraform"
+
+	ingress  { 
+		from_port ="${var.port}"
+		to_port="${var.port}"
+		protocol="tcp"
+		cidr_blocks=["0.0.0.0/0"]
+}
+}
